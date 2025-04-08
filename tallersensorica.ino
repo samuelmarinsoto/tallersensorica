bab9844ab591b6aa1c9abe634aa5c1c901434364
Samuel Marín Soto <samarin@estudiantec.cr>
2025-04-08 14:41:45 -0600

    agregar calculo para resistencia, imprimir timestamp (ms), señal, y resistencia en csv

--- tallersensorica.ino
+++ tallersensorica.ino
@@ -6,12 +6,17 @@ void setup() {
 
 void loop() {
     unsigned long timestamp = millis();  // Get time since Arduino started (ms)
-    int sensorValue = analogRead(puerto);  // Read KY-018 light sensor (0-1023)
+    int sensor_output = analogRead(puerto);  // Read KY-018 light sensor (0-1023)
 
-    // Print in CSV format: timestamp,sensor_value
-    Serial.print(timestamp);
+	float tension = sensor_output * (5.0/1023) * 1000; // desdigitalizar?
+	float resistance = 10000 * ( tension / ( 5000.0 - tension) ); // divisor de tension en reversa
+
+    // Print in CSV format: timestamp,sensor_output,resistance
+    Serial.print(timestamp); // milisegundos
     Serial.print(",");
-    Serial.println(sensorValue);
-
-    delay(1000);  // Log every second
+    Serial.print(sensor_output); // milivolts
+    Serial.print(",");
+	Serial.println(resistance); // Ohm
+	
+    delay(100);  // milliseconds
 }
